a1e317c3895e3e7e712f713be54e67c481944751
Jinnatul Ferdushy Khan <jinnatul.f.khan@gmail.com>
2024-01-17 11:16:34 +0100

    added localStorages

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,43 +1,60 @@
-import React from "react";
+
+import React, { Component } from "react";
 import Header from "./Header/Header";
 import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Checkout from "./Orders/Checkout/Checkout";
 import Orders from "./Orders/Orders";
 import Auth from "./Auth/Auth";
 import { connect } from "react-redux";
+import { authCheck } from "../redux/AuthActionCreator";
+
+
 
 const mapStateToProps = state => {
+
     return {
         token: state.token,
     }
 }
 
-const Main = props => {
-    let routes = null;
-    if (props.token === null) {
-        routes = (<Switch>
-
-            <Route path='/login' component={Auth} />
-
-        </Switch>
-
-        );
-    } else {
-        routes = (<Switch>
-            <Route path='/' component={BurgerBuilder} />
-            <Route path='/orders' component={Orders} />
-            <Route path='/checkout' component={Checkout} /></Switch>
-        )
+const mapDispatchToProps = dispatch => {
+    return {
+        authCheck: () => dispatch(authCheck()),
     }
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                {routes}
-            </div>
-        </div>
-    )
 }
 
-export default connect(mapStateToProps)(Main);
+class Main extends Component {
+    componentDidMount() {
+        this.props.authCheck();
+    }
+    render() {
+        let routes = null;
+        if (this.props.token === null) {
+            routes = (<Routes>
+                <Route path="/login" element={<Auth />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+            );
+        } else {
+            routes = (<Routes>
+                <Route path="/" element={<BurgerBuilder />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/checkout" element={<Checkout />} />
+
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>);
+        }
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    {routes}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- src/redux/AuthActionCreator.js
+++ src/redux/AuthActionCreator.js
@@ -1,6 +1,7 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
+
 export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
@@ -27,6 +28,25 @@ export const auth = (email, password, mode) => dispatch => {
     const API_KEY = "AIzaSyDyJUJ-eo4Fxxm0Ng0ITNk2XCXxj1GIqn4";
     axios.post(authUrl + API_KEY, authData)
         .then(response => {
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('userId', response.data.localId);
+            const expirationTime = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('expirationTime', expirationTime);
             dispatch(authSuccess(response.data.idToken, response.data.localId))
         })
+}
+
+export const authCheck = (dispatch) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        //logout
+    } else {
+        const expirationTime = new Date(localStorage.getItem('expirationTime'));
+        if (expirationTime <= new Date()) {
+            //logout
+        } else {
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId));
+        }
+    }
 }
